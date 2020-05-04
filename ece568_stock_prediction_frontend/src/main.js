d034ba2b08de764e326c54f98e964ac9d4a0ee31
hanmufu <hanmufu@gmail.com>
2020-05-03 21:59:18 -0400

    Implement router between pages

---
+++ ece568_stock_prediction_frontend/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+import VCharts from 'v-charts';
+Vue.use(VCharts);
+// import VeLine from 'v-charts/lib/candle.common'
+// Vue.component(VeLine.name, VeLine)
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+}).$mount('#app')
